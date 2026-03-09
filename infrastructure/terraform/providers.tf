c373577226bd7c1cc1b8a518333bec9576d10c4d
Michael Joshua Vargas <vargas.michaeljoshua@gmail.com>
2026-03-09 15:01:15 +0800

    terraform fmt

--- infrastructure/terraform/providers.tf
+++ infrastructure/terraform/providers.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     google = {
-        source = "hashicorp/google"
-        version = "7.16.0"
+      source  = "hashicorp/google"
+      version = "7.16.0"
     }
   }
 }
 
 provider "google" {
   project = var.project_id
-  region = var.region
+  region  = var.region
 }
