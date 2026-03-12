28261b3e455604a9623fef19a1f19f9dda267572
Michael Joshua Vargas <vargas.michaeljoshua@gmail.com>
2026-03-12 20:55:16 +0800

    day 4 tasks

---
+++ notebooks/market_data_prototype.py
@@ -0,0 +1,124 @@
+import marimo
+
+__generated_with = "0.20.4"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+
+    return
+
+
+@app.cell
+def _():
+    import yfinance as yf
+    import pandas as pd
+    from pathlib import Path
+
+    return Path, pd, yf
+
+
+@app.cell
+def _(Path):
+    # Configuration
+    TICKERS = ["AAPL", "MSFT", "GOOGL", "AMZN"]
+    START_DATE = "2023-01-01"
+    INTERVAL = "1h"
+
+    OUTPUT_DIR = Path("../data/raw/market_data")
+    OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+    return OUTPUT_DIR, START_DATE, TICKERS
+
+
+@app.cell
+def _(yf):
+    # Fetch market data
+    def fetch_market_data(tickers, start_date):
+        """
+        Pull stock data from Yahoo Finance
+        """
+        df = yf.download(
+            tickers,
+            start=start_date,
+            group_by="ticker",
+            auto_adjust=True,
+            threads=True
+        )
+    
+        return df
+
+    return (fetch_market_data,)
+
+
+@app.function
+# Convert to normalize dataframe
+def transform_data(df):
+    """
+    Convert multi-index dataframe to flat structure
+    """
+    df = df.stack(level=0).reset_index()
+
+    df = df.rename(
+        columns={
+            "level_1": "ticker",
+            "Date": "date"
+        }
+    )
+    
+    return df
+
+
+@app.cell
+def _(OUTPUT_DIR):
+    # Save Parquet
+    def save_parquet(df):
+        """
+        Save data locally as parquet
+        """
+        file_path = OUTPUT_DIR / "market_data.parquet"
+    
+        df.to_parquet(
+            path = file_path,
+            engine = "pyarrow",
+            index = False
+        )
+
+        return(f"Saved parquet to {file_path}")
+
+    return (save_parquet,)
+
+
+@app.cell
+def _(START_DATE, TICKERS, fetch_market_data, save_parquet):
+    print("Fetching market data...")
+
+    raw_df = fetch_market_data(TICKERS, START_DATE)
+
+    print("Transforming data...")
+
+    df = transform_data(raw_df)
+
+    print("Saving parquet...")
+
+    save_parquet(df)
+
+    print("Pipeline complete!")
+    return
+
+
+@app.cell
+def _(OUTPUT_DIR, pd):
+    market_df = pd.read_parquet(OUTPUT_DIR / "market_data.parquet")
+    market_df.head()
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
